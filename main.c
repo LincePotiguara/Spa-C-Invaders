3cbfe9dcf056691eb4947164f2a6b12d28bdd57f
Luan Martins <luanmufrj@hotmail.com>
2019-06-15 15:57:30 -0300

    Adição do jogador

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/* Variável responsável pela detecção do teclado */
+int ch;
+
+/* Coordenadas do jogador, coordenadas dos limites do terminal */
+int player_x, player_y, max_x, max_y;
+
+/* Strings responsáveis pelo jogador */
+char player_01[10] = "__|__", player_02[10]= "|_____|";
+
 /* Inicializa a tela */
 void initialize() {
     initscr();
@@ -14,21 +23,66 @@ void initialize() {
     noecho();
     keypad(stdscr, TRUE);
 
+    /* Deixa o cursor invisível */
+    curs_set(0);
+}
+
+/* Função responsável pelo controle do jogador */
+void player_control(){
+
+    /* Detecta a tecla pressionada no teclado */
+    ch = getch();
+    switch(ch){
+
+        /* Se for ESC, sai do jogo */
+        case ESC_KEY:
+            quit(stdscr);
+            break;
+
+        /* Se for a setinha esquerda, anda com o conjunto pra esquerda */
+        case LEFT_ARROW_KEY:
+            if (player_x > 0){
+                move(player_y, 0);
+                wclrtoeol(stdscr);
+                move(player_y-1, 0);
+                wclrtoeol(stdscr);
+                player_x--;
+            }
+            break;
+
+        /* Se for a setinha direita, anda com o conjunto pra direita */
+        case RIGHT_ARROW_KEY:
+            if (player_x < max_x - PLAYER_02_SIZE){
+                move(player_y, 0);
+                wclrtoeol(stdscr);
+                move(player_y-1, 0);
+                wclrtoeol(stdscr);
+                player_x++;
+            }
+            break;
+        }
+        refresh();
+        mvprintw(player_y-1, player_x+1, player_01);
+        mvprintw(player_y, player_x, player_02);
 }
 
 /* Executa até pressionar a tecla Esc */
 void execute_until_esc() {
-    int ch;
-    char keycode[17];
-    while ( (ch = getch()) != ESC_KEY ) {
-		snprintf(keycode, 16, "%d", ch);
-		move(0, 0);
-		clrtoeol();
-
-        /* Escreve o código da tecla na tela */
-		wprintw(stdscr, keycode);
-        refresh();
-
+    
+        /* Detecta e regista os limites do terminal */
+    getmaxyx(stdscr, max_y, max_x);
+    
+    /* Determina a poisção inicial da nave */
+    player_x = (max_x/2) - 3;
+    player_y = max_y - 3;
+    
+    /* Imprime a posição inicial da nave */
+    mvprintw(player_y-1, player_x+1, player_01);
+    mvprintw(player_y, player_x, player_02);
+    
+    while (1) {
+        /* Função responsável pelo controle do jogador */
+        player_control();
 	}
 }
 
--- main.h
+++ main.h
@@ -3,6 +3,13 @@
 #include <ncurses.h>
 
 #define ESC_KEY 27
+#define DOWN_ARROW_KEY 258
+#define UP_ARROW_KEY 259
+#define LEFT_ARROW_KEY 260
+#define RIGHT_ARROW_KEY 261
+#define PLAYER_01_SIZE 5
+#define PLAYER_02_SIZE 7
 void initialize();
 void execute_until_esc();
 void quit();
+void player_control();
