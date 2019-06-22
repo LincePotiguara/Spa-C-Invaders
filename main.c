2fd41b83e01d5cc614bb7b3ab7939e509180780f
Quintela <12141500+LincePotiguara@users.noreply.github.com>
2019-06-21 23:10:43 -0300

    Pequena mudança para adequação ao branch master

--- main.c
+++ main.c
@@ -1,13 +1,132 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ncurses.h>
+#include "sprites.h"
 #include "main.h"
+
+/*Definições dos sprites dos inimigos*/
+char enemy_type1[] = "***";
+
+/* Variável responsável pela detecção do teclado */
+int ch;
+
+/* Coordenadas do jogador, coordenadas dos limites do terminal */
+int player_x, player_y, max_x, max_y;
+
+/* Strings responsáveis pelo jogador */
+char player_01[10] = "__|__", player_02[10]= "|_____|";
 
 int main(int argc, char ** argv) {
     /* Inicializa a tela */
     initialize();
+
     /* Registra entrada de maneira não bloqueante */
     nodelay(stdscr, 1);
+
     /* Executa até pressionar a tecla Esc */
     execute_until_esc();
+    
     /* Libera a memória e finaliza o programa */
     quit();
     return 0;
+}
+
+/* Inicializa a tela */
+void initialize() {
+    initscr();
+
+    /* Checando por falhas */
+    if(stdscr == NULL) {
+        fprintf(stderr, "Ocorreu um erro ao inicializar ncurses\n");
+        exit(1);
+    }
+
+    /* Deixa de mostrar as teclas pressionadas e habilita as setas */
+    noecho();
+    keypad(stdscr, TRUE);
+
+    /* Deixa o cursor invisível */
+    curs_set(0);
+}
+
+/* Executa até pressionar a tecla Esc */
+void execute_until_esc() {
+
+        /* Detecta e regista os limites do terminal */
+    getmaxyx(stdscr, max_y, max_x);
+
+    /* Determina a poisção inicial da nave */
+    player_x = (max_x/2) - 3;
+    player_y = max_y - 3;
+
+    /* Imprime a posição inicial da nave */
+    mvprintw(player_y-1, player_x+1, player_01);
+    mvprintw(player_y, player_x, player_02);
+
+    /* Quantidade de inimigos por linha e de linhas de inimigos*/
+    int enemy_qty = 11;
+    int enemy_line = 5;
+
+    /*Imprime a posição inicial da wave de inimigos*/
+    print_wave(1, 1, enemy_qty, enemy_line, enemy_type1);
+
+    /*Move a wave de inimigos até 2 linhas antes do jogador*/
+    int line;
+    for(line = 1; line < player_y-enemy_line-6; line++){
+
+      move_wave(line, enemy_qty, enemy_line, max_x, 0.25*100000, enemy_type1);
+
+    }
+
+    while (1) {
+        /* Função responsável pelo controle do jogador */
+        player_control();
+
+	}
+}
+
+/* Função responsável pelo controle do jogador */
+void player_control(){
+
+    /* Detecta a tecla pressionada no teclado */
+    ch = getch();
+    switch(ch){
+
+        /* Se for ESC, sai do jogo */
+        case ESC_KEY:
+            quit();
+            break;
+
+        /* Se for a setinha esquerda, anda com o conjunto pra esquerda */
+        case LEFT_ARROW_KEY:
+            if (player_x > 0){
+                move(player_y, 0);
+                wclrtoeol(stdscr);
+                move(player_y-1, 0);
+                wclrtoeol(stdscr);
+                player_x--;
+            }
+            break;
+
+        /* Se for a setinha direita, anda com o conjunto pra direita */
+        case RIGHT_ARROW_KEY:
+            if (player_x < max_x - PLAYER_02_SIZE){
+                move(player_y, 0);
+                wclrtoeol(stdscr);
+                move(player_y-1, 0);
+                wclrtoeol(stdscr);
+                player_x++;
+            }
+            break;
+        }
+        refresh();
+        mvprintw(player_y-1, player_x+1, player_01);
+        mvprintw(player_y, player_x, player_02);
+}
+
+/* Sai da tela */
+void quit() {
+    delwin(stdscr);
+    endwin();
+    exit(0);
 }
--- makefile
+++ makefile
@@ -6,7 +6,7 @@ LFLAGS=-lncurses
 all: clean spac
 
 spac:
-	$(CC) -o spac main.c $(CFLAGS) $(LFLAGS)
+	$(CC) -o spac sprites.c main.c $(CFLAGS) $(LFLAGS)
 
 clean:
 	rm spac
